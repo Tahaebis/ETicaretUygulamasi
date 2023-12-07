ed44536eb301591f1eb057cabd4244717bd3a7ca
Taha EBİŞ <tahaebis0@gmail.com>
2023-12-07 10:45:22 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ETicaretUygulamasi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
