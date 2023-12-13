1928598838d021e4dd3f6b268fb7072838d8d03f
Murat Baseren <muratbaseren@gmail.com>
2023-12-13 12:05:26 +0300

    Admin de category işlemleri listeleme(index) ve Create ekranları yapıldı.

--- Areas/Admin/Controllers/CategoryController.cs
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -1,13 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ETicaretUygulamasi.Areas.Admin.Models.CategoryModels;
+using ETicaretUygulamasi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace ETicaretUygulamasi.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
+        DatabaseContext db;
+
+        public CategoryController(DatabaseContext _db)
+        {
+            db = _db;
+        }
+
         public IActionResult Index()
         {
+            List<Category> categories = db.Categories.ToList();
+
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CategoryCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+                Category category = new Category();
+                category.Name = model.Name;
+                category.Description = model.Description;
+                category.Hidden = model.Hidden;
+                category.Locked = model.Locked;
+                category.CreatedAt = DateTime.Now;
+                category.CreatedUserName = User.Identity.Name;
+
+                db.Categories.Add(category);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
         }
     }
 }
