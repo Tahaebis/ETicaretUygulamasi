c6b532f0281b12ee1f64a5b53b55306254e5ec14
Murat Baseren <muratbaseren@gmail.com>
2023-12-13 11:07:10 +0300

    Admin area oluşturuldu.

--- Program.cs
+++ Program.cs
@@ -49,6 +49,10 @@ namespace ETicaretUygulamasi
             app.UseAuthorization();
 
             app.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+            app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
