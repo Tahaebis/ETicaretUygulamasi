0d229dc24ee2c09e2db0a789764944ec1e4fa539
Murat Baseren <muratbaseren@gmail.com>
2023-12-14 20:32:08 +0300

    Kategori ekranları tamamlandı.

--- Areas/Admin/Controllers/CategoryController.cs
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -1,7 +1,6 @@
 ﻿using ETicaretUygulamasi.Areas.Admin.Models.CategoryModels;
 using ETicaretUygulamasi.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Storage;
 
 namespace ETicaretUygulamasi.Areas.Admin.Controllers
 {
@@ -47,6 +46,50 @@ namespace ETicaretUygulamasi.Areas.Admin.Controllers
             }
 
             return View(model);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Category category = db.Categories.Find(id);
+
+            CategoryEdit model = new CategoryEdit();
+            model.Name = category.Name;
+            model.Description = category.Description;
+            model.Locked = category.Locked;
+            model.Hidden = category.Hidden;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, CategoryEdit model)
+        {
+            if (ModelState.IsValid)
+            {
+                Category category = db.Categories.Find(id);
+
+                category.Name = model.Name;
+                category.Description = model.Description;
+                category.Locked = model.Locked;
+                category.Hidden = model.Hidden;
+                category.ModifiedAt = DateTime.Now;
+                category.ModifiedUserName = User.Identity.Name;
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Category category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Areas/Admin/Models/CategoryModels/CategoryEdit.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ETicaretUygulamasi.Areas.Admin.Models.CategoryModels
+{
+    public class CategoryEdit
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(4000)]
+        public string? Description { get; set; }
+        public bool Hidden { get; set; }
+        public bool Locked { get; set; }
+    }
+}
